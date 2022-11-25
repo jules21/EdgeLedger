0213762b086a67916f6f498e578531eb860e9052
jules fabien <julesfabien96@gmail.com>
2022-11-25 18:58:02 +0200

    animate use AOS

--- js/app.js
+++ js/app.js
@@ -20,12 +20,12 @@ window.addEventListener('scroll', function(){
     }
 })
 
-$('#navbar a, .btn').on('click', function(e){
-    if(this.hash !== ''){
-        e.preventDefault();
-    }
-    const hash = this.hash
-    $('html, body').animate({
-        scrollTop :$(hash).offset().top  - 100
-    }, 800)
-})
+// $('#navbar a, .btn').on('click', function(e){
+//     if(this.hash !== ''){
+//         e.preventDefault();
+//     }
+//     const hash = this.hash
+//     $('html, body').animate({
+//         scrollTop :$(hash).offset().top  - 100
+//     }, 800)
+// })
