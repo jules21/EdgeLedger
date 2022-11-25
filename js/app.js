9ba883f519495dc384b09257df26dc3f49e7eb26
jules fabien <julesfabien96@gmail.com>
2022-11-25 18:50:02 +0200

    add smooth scroll with jquery

--- js/app.js
+++ js/app.js
@@ -7,6 +7,25 @@ function initMap (){
 
     const marker = new google.maps.Marker({
         position:home,
-        map:ma
+        map
     })
 }
+
+window.addEventListener('scroll', function(){
+    console.log(window.screenY);
+    if(window.scrollY > 150){
+        document.getElementById('navbar').style.opacity = 0.6
+    }else{
+        document.getElementById('navbar').style.opacity = 1
+    }
+})
+
+$('#navbar a, .btn').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+    }
+    const hash = this.hash
+    $('html, body').animate({
+        scrollTop :$(hash).offset().top  - 100
+    }, 800)
+})
